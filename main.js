b76adc9ee50f33e711b2a9c1fca8daacb982faa6
Bruce Clark <bruce.clark55@gmail.com>
2023-10-29 13:45:59 -0400

    added data

---
+++ main.js
@@ -0,0 +1,22 @@
+const students = [
+    {
+        id: 1,
+        name: "Kang the Conqueror",
+        house: "gryffindor",
+    },
+    {
+        id: 2,
+        name: "Victor Timely",
+        house: "hufflepuff",
+    },
+    {
+        id: 3,
+        name: "Immortus",
+        house: "ravenclaw",
+    },
+    {
+        id: 4,
+        name: "RamaTut",
+        house: "slytherin",
+    }
+]
